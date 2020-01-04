2965ebe49584b5af4bc2910b4dfa99788a1c9dc4
admin <kci2003@mail.ru>
2020-01-04 17:39:06 +0500

    dictionary

--- dictionay3.py
+++ dictionay3.py
@@ -1,11 +1,13 @@
 n = int(0)
-a = []
 m = [[]]
 with open ('C:\\temp\\text3.txt') as f:
     for line in f:
         a = line.lower().strip().split(';')
-        print(a)
-        #m[[n]]+ = a
+        if n == 0:
+            m[0]+=a
+        else:
+            m.append(a)
         n += 1
+# выше занесли текст в двумерную матрицу дальше будем обрабатывать двумерный список
 print(m)
-print(n)
+
